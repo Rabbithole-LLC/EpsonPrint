85f90cda83b4e488d38595647fc4e8ee0addaae8
Rundaingne <48364843+Rundaingne@users.noreply.github.com>
2023-05-16 20:32:13 -0600

    Removed nested git repo

--- Package.swift
+++ Package.swift
@@ -6,18 +6,13 @@ import PackageDescription
 let package = Package(
     name: "EpsonPrint",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "EpsonPrint",
-            targets: ["EpsonPrint"]),
+            targets: ["Libepos2"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "EpsonPrint", url: "https://flippos.s3.us-west-1.amazonaws.com/public/libepos2.xcframework.zip", checksum: "c95fe6252b37dbb8a5e91df3fe5fc98771ffa9c60f13fa944579098f728c9bfa")
+        .target(name: "EpsonPrint"),
+        .binaryTarget(name: "Libepos2", path: "libepos2.xcframework")
     ]
 )
